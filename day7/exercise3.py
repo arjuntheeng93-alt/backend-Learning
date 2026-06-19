ab3e3a489a8759e0f54daf5625175a381d65a032
Arjun <arjuntheeng93@gmail.com>
2026-06-19 12:04:42 +0545

    day7: OOP - classes, properties, class methods

---
+++ day7/exercise3.py
@@ -0,0 +1,56 @@
+class BankAccount: # we have create the class as bankaacount
+    def __init__(self, owner, balance=0): 
+        self.owner = owner
+        elf._balance = balance 
+    
+    def deposite(self, amount):
+        if amount < 0:
+            raise ValueError("Deposite money should be in 1")
+        self._balance += amount
+        return self._balance
+    def withdraw(self, amount):
+        if amount <= 0:
+            raise ValueError("withdraw amount must be in positive")
+        if amount > self._balance:
+            raise ValueError("Insufficiant fund")
+        self._balance  -= amount
+        return self._balance
+    @property
+    def balance(self):
+        return self._balance
+    
+    def __str__(self):
+        return f"BankAccount (owner= {self.owner}, balance={self._balance})"
+    
+    def __repr__(self):
+        return f"BankAccount(owner= {self.owner}, balance=(self._balance)"
+
+    @classmethod
+    def create_saving(cls, owner):
+        """factory method - creates a saving account with 100 bonus"""
+        account = cls(owner, balance = 100)
+        return account    
+    @staticmethod
+    def validate_amount(amount):
+        """Validate that amount is positive number"""
+        if not isinstance(amount, (int, float)):
+            raise TypeError("Amount must be a number")
+        if amount <= 0:
+            raise ValueError("Amount must be positve")
+account = BankAccount("Arjun", 1000)
+print(account)
+account.deposite(1000)
+print(account.balance)
+account.withdraw(500)
+print(account.balance)
+
+try:
+    account.withdraw(9999)
+except ValueError as e:
+    print(f"Error:{e}")         
+     
+savings = BankAccount.create_saving("Arjun")
+print(savings.balance)
+
+BankAccount.validate_amount(500)
+BankAccount.validate_amount(-10)
